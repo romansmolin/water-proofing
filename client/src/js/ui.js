7c78f44147f62aff0f945fa5643caac6dcd956e2
Roman Smolin <romasmolin29@gmail.com>
2023-12-20 17:11:51 +0200

    Fix acardion

--- client/src/js/ui.js
+++ client/src/js/ui.js
@@ -2,8 +2,17 @@
 const preheader = document.querySelector('.preheader')
 const preheaderIcon = document.querySelector('.preheader__text .close-icon');
 
-preheaderIcon.addEventListener('click', () => {
-    if (preheader) {
-        preheader.remove();
-    }
-})
+// preheaderIcon.addEventListener('click', () => {
+//     if (preheader) {
+//         preheader.remove();
+//     }
+// })
+
+const labels = document.querySelectorAll('.faq-section__accardion__tab__label');
+
+// labels.forEach(label => {
+//     label.addEventListener('click', (e) => {
+//         const forAttribute = label.getAttribute('for');
+//         console.log('for attribute:', forAttribute);
+//     });
+// });
